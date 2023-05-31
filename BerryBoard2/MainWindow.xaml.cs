5076a46a6586d27f05455d1aeff03d6daa6edbb3
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2023-05-31 17:24:33 -0400

    bunch of improvements

--- BerryBoard2/MainWindow.xaml.cs
+++ BerryBoard2/MainWindow.xaml.cs
@@ -24,9 +24,13 @@ namespace BerryBoard2
 	{
 		private Controller? controller;
 
+        private const string paramText = "Parameter";
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+            ParamText.Text = paramText;
 		}
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -81,15 +85,65 @@ namespace BerryBoard2
             {
                 selectedButton.Background = (SolidColorBrush)FindResource("ButtonBackground");
             }
-            else ClearButton.IsEnabled = true;
 
             button.Background = Brushes.DarkOrange;
             selectedButton = button;
 
             // Load data
             ButtonAction? data = controller?.GetButtonAction(int.Parse(button.Tag.ToString()));
-            ActionLabel.Text = data.action.ToString();
+
+            if (data.action != Model.Action.None)
+			{
+                ClearButton.IsEnabled = true;
+                foreach (TreeViewItem item in MainTreeView.Items)
+                {
+                    string header = GetHeaderByAction(item, data.action);
+                    if (header != null)
+                    {
+                        ActionLabel.Text = header;
+                        break;
+                    }
+                }
+            }
+            else
+			{
+                ActionLabel.Text = data.action.ToString();
+                ClearButton.IsEnabled = false;
+            }
             ParamTextbox.Text = data.param.ToString();
+
+            switch (data.action)
+			{
+                case Model.Action.ChangeScene:
+                    ParamText.Text = "Scene Name";
+                    ParamTextbox.IsEnabled = true;
+                    break;
+                case Model.Action.StartProcess:
+                    ParamText.Text = "Filepath";
+                    ParamTextbox.IsEnabled = true;
+                    break;
+                default:
+                    ParamText.Text = paramText;
+                    ParamTextbox.IsEnabled = false;
+                    break;
+            }
+        }
+
+        private string GetHeaderByAction(TreeViewItem item, Model.Action action)
+        {
+            if ((string)item.Tag == action.ToString())
+                return (string)item.Header;
+
+            string foundHeader = null;
+
+            foreach (TreeViewItem child in item.Items)
+            {
+                foundHeader = GetHeaderByAction(child, action);
+                if (foundHeader != null)
+                    break;
+            }
+
+            return foundHeader;
         }
 
         private void Button_Drop(object sender, DragEventArgs e)
@@ -115,8 +169,12 @@ namespace BerryBoard2
 		{
             if (selectedButton != null)
 			{
-                Button button = (Button)sender;
                 controller?.ClearButton(int.Parse(selectedButton.Tag.ToString()));
+                ActionLabel.Text = "None";
+                ParamText.Text = paramText;
+                ParamTextbox.Text = string.Empty;
+                ClearButton.IsEnabled = false;
+                ParamTextbox.IsEnabled = false;
             }
 		}
 	}
