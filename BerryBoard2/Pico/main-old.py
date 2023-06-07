f8ee321e97ce38eae1b1cc8cc33a3fe668b1a694
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2023-06-07 16:11:13 -0400

    fixed window animations, improved pico script

---
+++ BerryBoard2/Pico/main-old.py
@@ -0,0 +1,60 @@
+from machine import Pin, UART
+import utime
+import sys
+import select
+
+# Define the pins for the columns and rows
+column_pins = [6, 7, 8, 9]
+row_pins = [20, 19, 18]
+led = Pin(25, Pin.OUT)
+
+# Create Pin objects for each pin, columns as output and rows as input with pull-down
+columns = [Pin(pin, Pin.OUT) for pin in column_pins]
+rows = [Pin(pin, Pin.IN, Pin.PULL_DOWN) for pin in row_pins]
+
+button_count = len(columns) * len(rows)
+buttons = [False] * button_count
+history = [False] * button_count
+
+blink_interval = 1
+blink_timer = blink_interval
+interval = 0.01
+while True:
+    for c, column in enumerate(columns):
+        column.value(1)
+        
+        for r, row in enumerate(rows):
+            button_index = r * len(columns) + c
+            buttons[button_index] = row.value() == 1
+
+        column.value(0)
+    
+    for i in range(button_count):
+        if buttons[i] != history[i]:
+            if buttons[i]:
+                print(i)
+                #print(f"Button {i + 1} was pressed")
+            #else:
+                #print(f"Button {i + 1} was released")
+            history[i] = buttons[i]
+            
+        # Handle LED blinking
+    blink_timer -= interval
+    if (blink_timer <= 0.0):
+        led.toggle()
+        blink_timer = blink_interval
+    
+    utime.sleep(interval)
+    
+"""
+# Define pins
+output_pin = Pin(9, Pin.OUT)
+input_pin = Pin(20, Pin.IN, Pin.PULL_DOWN)
+
+# Set the output pin to high
+output_pin.value(1)
+
+while True:
+    print(input_pin.value())
+    utime.sleep(1)
+"""
--- BerryBoard2/Pico/main.py
+++ BerryBoard2/Pico/main.py
@@ -1,60 +1,76 @@
-from machine import Pin, UART
+from machine import Pin
 import utime
-import sys
-import select
 
-# Define the pins for the columns and rows
+class BlinkingLED:
+    def __init__(self, pin, blink_interval):
+        self.led = Pin(pin, Pin.OUT)
+        self.blink_interval = blink_interval
+        self.blink_timer = blink_interval
+
+    def update(self, interval):
+        self.blink_timer -= interval
+        if self.blink_timer <= 0.0:
+            self.led.toggle()
+            self.blink_timer = self.blink_interval
+
+class ButtonGrid:
+    def __init__(self, column_pins, row_pins):
+        self.columns = [Pin(pin, Pin.OUT) for pin in column_pins]
+        self.rows = [Pin(pin, Pin.IN, Pin.PULL_DOWN) for pin in row_pins]
+        self.button_count = len(self.columns) * len(self.rows)
+        self.buttons = [False] * self.button_count
+        self.history = [False] * self.button_count
+
+    def update(self):
+        for c, column in enumerate(self.columns):
+            column.value(1)
+
+            for r, row in enumerate(self.rows):
+                button_index = r * len(self.columns) + c
+                self.buttons[button_index] = row.value() == 1
+
+            column.value(0)
+
+        for i in range(self.button_count):
+            if self.buttons[i] != self.history[i]:
+                if self.buttons[i]:
+                    print(i)
+                self.history[i] = self.buttons[i]
+
+class RotaryEncoder:
+    def __init__(self, clk_pin, dt_pin):
+        self.clk = Pin(clk_pin, Pin.IN, Pin.PULL_UP)
+        self.dt = Pin(dt_pin, Pin.IN, Pin.PULL_UP)
+        self.counter = 0
+        self.clkLastState = self.clk.value()
+
+    def update(self):
+        clkState = self.clk.value()
+        if clkState != self.clkLastState:
+            if self.dt.value() != clkState:
+                self.counter += 1
+            else:
+                self.counter -= 1
+            print('Rotary Counter:', self.counter)
+        self.clkLastState = clkState
+
+# Define pins for buttons
 column_pins = [6, 7, 8, 9]
 row_pins = [20, 19, 18]
-led = Pin(25, Pin.OUT)
+button_grid = ButtonGrid(column_pins, row_pins)
 
-# Create Pin objects for each pin, columns as output and rows as input with pull-down
-columns = [Pin(pin, Pin.OUT) for pin in column_pins]
-rows = [Pin(pin, Pin.IN, Pin.PULL_DOWN) for pin in row_pins]
+# Define rotary encoders
+rotary1 = RotaryEncoder(1, 2)
+rotary2 = RotaryEncoder(3, 4)
 
-button_count = len(columns) * len(rows)
-buttons = [False] * button_count
-history = [False] * button_count
+# Define LED
+led = BlinkingLED(25, 1.0)
 
-blink_interval = 1
-blink_timer = blink_interval
 interval = 0.01
 while True:
-    for c, column in enumerate(columns):
-        column.value(1)
-        
-        for r, row in enumerate(rows):
-            button_index = r * len(columns) + c
-            buttons[button_index] = row.value() == 1
+    button_grid.update()
+    rotary1.update()
+    rotary2.update()
+    led.update(interval)
 
-        column.value(0)
-    
-    for i in range(button_count):
-        if buttons[i] != history[i]:
-            if buttons[i]:
-                print(i)
-                #print(f"Button {i + 1} was pressed")
-            #else:
-                #print(f"Button {i + 1} was released")
-            history[i] = buttons[i]
-            
-        # Handle LED blinking
-    blink_timer -= interval
-    if (blink_timer <= 0.0):
-        led.toggle()
-        blink_timer = blink_interval
-    
     utime.sleep(interval)
-    
-"""
-# Define pins
-output_pin = Pin(9, Pin.OUT)
-input_pin = Pin(20, Pin.IN, Pin.PULL_DOWN)
-
-# Set the output pin to high
-output_pin.value(1)
-
-while True:
-    print(input_pin.value())
-    utime.sleep(1)
-"""
